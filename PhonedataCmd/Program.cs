fa45efd02b9a8cd9058828d4ee6aff4d413abe12
Barry_yue <yuechangyue@live.com>
2019-09-12 11:43:02 +0800

    Update Program.cs

--- PhonedataCmd/Program.cs
+++ PhonedataCmd/Program.cs
@@ -9,12 +9,12 @@ namespace PhonedataCmd
         {
             PhoneData pd = new PhoneData("phone.dat");
             string output;
-            output = pd.Lookup("14794299760").ToString();
+            output = pd.Lookup("1325444878").ToString();
             Console.WriteLine(output);
             Console.ReadKey();
             /*
-             * 1344710142
-             * 1344711000
+             * 1325444878
+             * 1325444878
              */
         }
     }
