3b8a99d15d9bbe9ff6dcf23f1dbe0cbdae9265b5
岳长月 <yuechangyue@live.com>
2019-09-12 11:13:41 +0800

    Add project files.

---
+++ PhonedataBenchmarkCore/Program.cs
@@ -0,0 +1,40 @@
+﻿using PhoneataCore;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PhonedataBenchmarkCore
+{
+    class Program
+    {
+        static string[] s;
+        static void Benchmark_add_element()
+        {
+            s = File.ReadAllLines("test.txt");
+        }
+        static void Benchmark_test()
+        {
+            PhoneData pd = new PhoneData("phone.dat");
+            DateTime dt1 = DateTime.Now;
+            Parallel.ForEach(s, (i) =>
+            {
+                pd.Lookup(i);
+
+            });
+            //foreach (var item in s)
+            //{
+            //    pd.Lookup(item);
+            //}
+            DateTime dt2 = DateTime.Now;
+            Console.WriteLine("并行计算 {0}个手机号码的查找，用时： {1}毫秒。\n", s.Length, (dt2 - dt1).TotalMilliseconds);
+
+
+        }
+        static void Main(string[] args)
+        {
+            Benchmark_add_element();
+            Benchmark_test();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PhonedataCmd/Program.cs
@@ -0,0 +1,21 @@
+﻿using PhoneataCore;
+using System;
+
+namespace PhonedataCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneData pd = new PhoneData("phone.dat");
+            string output;
+            output = pd.Lookup("14794299760").ToString();
+            Console.WriteLine(output);
+            Console.ReadKey();
+            /*
+             * 1344710142
+             * 1344711000
+             */
+        }
+    }
+}
